fdf9680187cd1b87695e3b06e6066aacb78c8932
tarsem singh <ts634629@gmail.com>
2024-03-19 15:49:57 +0530

    auth

--- app.js
+++ app.js
@@ -9,28 +9,6 @@ const authRoutes = require('./routes/auth.routes')
 app.use('/api/v1/user', authRoutes)
 
 
-function authentication(req, res, next) {
-    const authheader = req.headers.authorization;
-    console.log(req.headers);
- 
-    if (!authheader) {
-        let err = new Error('You are not authenticated!');
-        res.setHeader('WWW-Authenticate', 'Basic');
-        err.status = 401;
-        return next(err)
-    }
- 
-    const auth = new Buffer.from(authheader.split(' ')[1],
-        'base64').toString().split(':');
-    const user = auth[0];
-    const pass = auth[1];
- 
-    
- 
-}
- 
-app.use(authentication)
-
 
 const productRoute = require('./routes/product.routes')
 app.use('/api/v1/product', productRoute)
