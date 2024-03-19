72d0f1ba4fc2e7e82981aa7d5deed8e610464ca3
tarsem singh <126371735+devtarsem@users.noreply.github.com>
2024-03-19 20:09:52 +0530

    Update py.py

--- controller/py.py
+++ controller/py.py
@@ -1,7 +1,7 @@
 from twilio.rest import Client
 
-account_sid = 'AC7526bd4af40d83f63312572f54b61c58'
-auth_token = '76ad6b8190c3cf2b40d1ba55eb21b742'
+# account_sid = 'AC7526bd4af40d83f63312572f54b61c58'
+# auth_token = '76ad6b8190c3cf2b40d1ba55eb21b742'
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
