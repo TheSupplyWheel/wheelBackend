dfceda82c16db22a2772728afdd0a8f78b00b8d4
tarsem singh <ts634629@gmail.com>
2025-01-09 21:18:41 +0530

    port

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const connection = mongoose.connect(process.env.CONNECTION).then(el=>{
     console.log('connection establish')
 })
 
-const server = app.listen(process.env.PORT, (req, res)=>{
+const port = process.env.PORT
+
+const server = app.listen(port, (req, res)=>{
     console.log(`The Backend is working at ${process.env.PORT}`)
 })
 server.requestTimeout = 30000;
