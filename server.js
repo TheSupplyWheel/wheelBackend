840c764376bcd01ee0ab959283129877d136860f
tarsem singh <ts634629@gmail.com>
2025-01-09 21:21:39 +0530

    configs-

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require('./app')
 
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
-dotenv.config({path : './environment/config.env'})
+dotenv.config({path : './config.env'})
 
 const connection = mongoose.connect(process.env.CONNECTION).then(el=>{
     console.log('connection establish')
